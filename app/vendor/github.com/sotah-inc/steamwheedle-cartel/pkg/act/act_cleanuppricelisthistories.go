913c510326ee82b4ee756df827416d8353bb3780
Adrian Parker <ihsw.aparker@gmail.com>
2019-08-09 15:28:45 -0700

    Update to latest dep.

--- app/vendor/github.com/sotah-inc/steamwheedle-cartel/pkg/act/act_cleanuppricelisthistories.go
+++ app/vendor/github.com/sotah-inc/steamwheedle-cartel/pkg/act/act_cleanuppricelisthistories.go
@@ -6,17 +6,17 @@ import (
 	"github.com/sotah-inc/steamwheedle-cartel/pkg/util"
 )
 
-type DownloadAuctionsInJob struct {
+type CleanupPricelistHistoriesInJob struct {
 	sotah.RegionRealmTuple
 }
 
-type DownloadAuctionsOutJob struct {
+type CleanupPricelistHistoriesOutJob struct {
 	sotah.RegionRealmTuple
 	Data ResponseMeta
 	Err  error
 }
 
-func (job DownloadAuctionsOutJob) ToLogrusFields() logrus.Fields {
+func (job CleanupPricelistHistoriesOutJob) ToLogrusFields() logrus.Fields {
 	return logrus.Fields{
 		"error":  job.Err.Error(),
 		"region": job.RegionName,
@@ -24,17 +24,17 @@ func (job DownloadAuctionsOutJob) ToLogrusFields() logrus.Fields {
 	}
 }
 
-func (c Client) DownloadAuctions(regionRealms sotah.RegionRealms) chan DownloadAuctionsOutJob {
+func (c Client) CleanupPricelistHistories(regionRealms sotah.RegionRealms) chan CleanupPricelistHistoriesOutJob {
 	// establishing channels
-	in := make(chan DownloadAuctionsInJob)
-	out := make(chan DownloadAuctionsOutJob)
+	in := make(chan CleanupPricelistHistoriesInJob)
+	out := make(chan CleanupPricelistHistoriesOutJob)
 
 	// spinning up the workers
 	worker := func() {
 		for inJob := range in {
 			body, err := inJob.RegionRealmTuple.EncodeForDelivery()
 			if err != nil {
-				out <- DownloadAuctionsOutJob{
+				out <- CleanupPricelistHistoriesOutJob{
 					RegionRealmTuple: inJob.RegionRealmTuple,
 					Data:             ResponseMeta{},
 					Err:              err,
@@ -45,7 +45,7 @@ func (c Client) DownloadAuctions(regionRealms sotah.RegionRealms) chan DownloadA
 
 			actData, err := c.Call("/", "POST", []byte(body))
 			if err != nil {
-				out <- DownloadAuctionsOutJob{
+				out <- CleanupPricelistHistoriesOutJob{
 					RegionRealmTuple: inJob.RegionRealmTuple,
 					Data:             ResponseMeta{},
 					Err:              err,
@@ -54,7 +54,7 @@ func (c Client) DownloadAuctions(regionRealms sotah.RegionRealms) chan DownloadA
 				continue
 			}
 
-			out <- DownloadAuctionsOutJob{
+			out <- CleanupPricelistHistoriesOutJob{
 				RegionRealmTuple: inJob.RegionRealmTuple,
 				Data:             actData,
 				Err:              nil,
@@ -64,13 +64,13 @@ func (c Client) DownloadAuctions(regionRealms sotah.RegionRealms) chan DownloadA
 	postWork := func() {
 		close(out)
 	}
-	util.Work(8, worker, postWork)
+	util.Work(64, worker, postWork)
 
 	// queueing up the regions
 	go func() {
 		for regionName, realms := range regionRealms {
 			for _, realm := range realms {
-				in <- DownloadAuctionsInJob{
+				in <- CleanupPricelistHistoriesInJob{
 					RegionRealmTuple: sotah.RegionRealmTuple{
 						RegionName: string(regionName),
 						RealmSlug:  string(realm.Slug),
