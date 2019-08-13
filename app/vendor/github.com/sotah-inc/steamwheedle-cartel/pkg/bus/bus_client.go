cc99a4831a15739ae724b4251dd833751bf932ae
Adrian Parker <ihsw.aparker@gmail.com>
2019-08-13 07:40:41 -0700

    Update to latest dep.

--- app/vendor/github.com/sotah-inc/steamwheedle-cartel/pkg/bus/bus_client.go
+++ app/vendor/github.com/sotah-inc/steamwheedle-cartel/pkg/bus/bus_client.go
@@ -572,6 +572,19 @@ func (r CheckSubscriptionsResults) TopicNames() []string {
 	return out
 }
 
+func (r CheckSubscriptionsResults) WithoutSubscriptions() CheckSubscriptionsResults {
+	out := CheckSubscriptionsResults{}
+	for _, result := range r {
+		if result.HasSubscriptions {
+			continue
+		}
+
+		out = append(out, result)
+	}
+
+	return out
+}
+
 type CheckSubscriptionsOutJob struct {
 	Err              error
 	TopicName        string
@@ -653,7 +666,7 @@ func (c Client) CheckAllSubscriptions() (CheckSubscriptionsResults, error) {
 				continue
 			}
 
-			in <- next.String()
+			in <- next.ID()
 		}
 
 		close(in)
