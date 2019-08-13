9b67a6ef6813784aaa8060942670477351e02694
Adrian Parker <ihsw.aparker@gmail.com>
2019-08-13 07:07:39 -0700

    Update to latest dep.

--- app/go.mod
+++ app/go.mod
@@ -4,7 +4,7 @@ go 1.12
 
 require (
 	github.com/sirupsen/logrus v1.4.2
-	github.com/sotah-inc/steamwheedle-cartel v0.0.0-20190813134854-facd15217216
+	github.com/sotah-inc/steamwheedle-cartel v0.0.0-20190813140727-be8d8fb4c383
 	github.com/twinj/uuid v1.0.0
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
--- app/vendor/github.com/sotah-inc/steamwheedle-cartel/pkg/bus/bus_client.go
+++ app/vendor/github.com/sotah-inc/steamwheedle-cartel/pkg/bus/bus_client.go
@@ -47,8 +47,12 @@ func (c Client) Topic(topicName string) *pubsub.Topic {
 	return c.client.Topic(topicName)
 }
 
-func (c Client) TopicNames() *pubsub.TopicIterator {
+func (c Client) Topics() *pubsub.TopicIterator {
 	return c.client.Topics(c.context)
+}
+
+func (c Client) Subscriptions(t *pubsub.Topic) *pubsub.SubscriptionIterator {
+	return t.Subscriptions(c.context)
 }
 
 func (c Client) FirmTopic(topicName string) (*pubsub.Topic, error) {
